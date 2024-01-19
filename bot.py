7091c937a327018347c89ceca158170421aa5566
Shrey Vishen <shrey.vishen@gmail.com>
2024-01-18 21:50:17 -0800

    reset

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ import io
 import time
 load_dotenv()
 response_times = {}
+TOKEN = os.getenv("BOT_TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 async def send_message(message, user_message, is_private):
     try:
         user_id = str(message.author.id)
@@ -18,22 +22,16 @@ async def send_message(message, user_message, is_private):
                 return
         response_times[user_id] = time.time()
         response = responses.handle_response(user_message)
-        if isinstance(response, str):
-            await message.author.send(response) if is_private else await message.channel.send(response)
-        else:
-            image_data = np.array(response, dtype=np.uint8)
-            img_byte_array = io.BytesIO()
-            Image.fromarray(image_data, 'RGB').save(img_byte_array, format='PNG')
-            img_byte_array.seek(0)
-            await message.author.send(file=discord.File(img_byte_array, filename='image.png')) if is_private else await message.channel.send(file=discord.File(img_byte_array, filename='image.png'))
+        image_data = np.array(response, dtype=np.uint8)
+        img_byte_array = io.BytesIO()
+        Image.fromarray(image_data, 'RGB').save(img_byte_array, format='PNG')
+        img_byte_array.seek(0)
+        await message.author.send(file=discord.File(img_byte_array, 'generated_image.png')) if is_private else await message.channel.send(file=discord.File(img_byte_array, 'generated_image.png'))
+
     except Exception as e:
         print(e)
 
 def run_discord_bot():
-    TOKEN = os.getenv("BOT_TOKEN")
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
@@ -44,7 +42,6 @@ def run_discord_bot():
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-        print(f"{username}: {user_message} ({channel})")
         if user_message:
             if user_message[0] == "!":
                 user_message = user_message[1:]
--- responses.py
+++ responses.py
@@ -2,9 +2,7 @@ import tensorflow as tf
 interpreter = tf.lite.Interpreter(model_path='human_face_generator.tflite')
 def handle_response(message) -> str:
     message = message.lower()
-    if message == "how":
-        return "working"
-    elif message == "generate":
+    if message == "generate":
         interpreter.allocate_tensors()
         random_noise = tf.random.normal(shape=(1, 100))
         input_tensor = interpreter.tensor(interpreter.get_input_details()[0]['index'])
