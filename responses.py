9165ec14c18ec86a442045e49448e27b6d3ef546
shreyvish5678 <shrey.vishen@gmail.com>
2024-01-21 13:13:36 -0800

    loaded .env for responses.py

--- responses.py
+++ responses.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import json
 import redis
 import os
+from dotenv import load_dotenv
+load_dotenv()
 interpreter = tf.lite.Interpreter(model_path='human_face_generator.tflite')
 redis_client = redis.StrictRedis(host="usw1-major-platypus-33840.upstash.io", port='33840', password=os.getenv("PASSWORD"), decode_responses=True)
 def handle_response(message) -> str:
